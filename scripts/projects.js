e2b27f0dd448b8b170b0bc219eaee5d583344a6f
Kyle Richey <richey.kyle.i@gmail.com>
2017-08-11 19:01:33 -0700

    got the raw data array finished

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,10 +1,20 @@
 'use strict';
 
-// var myProjects = [
-//   {
-//     name: 'Cthulhu Bedtime Stories',
-//     url: 'https://krichey15.github.io/cthulhu-bedtime/',
-//     describe: '<p>A interactive story game I created in colaboration fellow  Code 201 students'
-//   }
-// ];
-// console.log(myProjects);
+var myProjects = [
+  {
+    name: 'Cthulhu Bedtime Stories',
+    url: 'https://krichey15.github.io/cthulhu-bedtime/',
+    describe: '<p>A interactive story game I created in colaboration fellow  Code 201 students'
+  },
+  {
+    name: 'About Me',
+    url: 'https://krichey15.github.io/about-me/',
+    describe: '<p>A very basic About-me page I created as a starting project in my 201 course</p>'
+  },
+  {
+    name: 'Code 101 Trial',
+    url: 'https://krichey15.github.io/',
+    describe: 'My first ever attempt to make a webpage live(still trying to figure out why it is not.)'
+  }
+];
+console.log(myProjects);
