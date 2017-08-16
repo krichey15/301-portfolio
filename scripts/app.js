7dd9a7b7187844d5a7a66fef16f423c5f147735e
Kyle Richey <richey.kyle.i@gmail.com>
2017-08-15 23:07:57 -0700

    finished mvp for class 2

--- scripts/app.js
+++ scripts/app.js
@@ -8,3 +8,11 @@ function Project(myProjects){
   this.describe = myProjects.describe;
 }
 console.log(projectArray);
+
+Project.prototype.toHTML = function() {
+  var $newProject = $('div.thumbnail-container').clone();
+
+  $newProject.find('h1').text(this.name);
+  $newProject.find('iframe').attr('src', this.url);
+  $newProject.find('p').text(this.describe);
+}
