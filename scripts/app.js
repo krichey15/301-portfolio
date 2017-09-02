39c02d164a03e1f5049351861e4e073559f44587
Kyle Richey <richey.kyle.i@gmail.com>
2017-09-01 21:45:45 -0700

    getting data from other sites but not seeing it on actual page

--- scripts/app.js
+++ scripts/app.js
@@ -10,16 +10,20 @@ function Project(rawDataProj){
 console.log(projects);
 
 Project.prototype.toHtml = function () {
-  var $newProject = $('div.thumbnail-container .thumbnail').clone();
+  var $newProject = $('div.thumbnail-container').clone();
   $newProject.removeClass('thumbnail-container');
 
-  $newProject.find('.thumbnail h1').html(this.name);
-  $newProject.find('.thumbnail iframe').attr('src', this.url);
-  $newProject.find('.thumbnail p').html(this.describe);
+  $newProject.find('h1').text(this.name);
+  $newProject.find('iframe').attr('src', this.url);
+  $newProject.find('p').text(this.describe);
 
   return $newProject;
 }
 
 myProjects.forEach(function(projectObject){
   projects.push(new Project(projectObject));
+});
+
+projects.forEach(function(project) {
+  $('.thumbnail').append(project.toHtml());
 });
