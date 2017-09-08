3598e0c67959406e97b2f3376d86d1a4a6441259
Kyle Richey <richey.kyle.i@gmail.com>
2017-09-07 17:44:32 -0700

    set up some routes

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
+app.get('/home', function (request, response){
+  response.sendFile('index.html', {root: './public'});
+})
+
 app.listen(PORT, function(){
   console.log(`Server started on port ${PORT}!`);
 })
